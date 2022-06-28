c2b4c7ea6774b0d22a14596b7268d3740ba910ff
desai <desaigunavant3219@gmail.com>
2022-06-28 08:48:26 +0530

    Resume maker 28-6-22

---
+++ app/src/main/java/com/example/resume/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.resume;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btncreate,btnedit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initBinding();
+        initOnClick();
+    }
+    void initBinding()
+    {
+        btncreate=findViewById(R.id.btncreate);
+        btnedit=findViewById(R.id.btnedit);
+    }
+    void initOnClick()
+    {
+        btncreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        btnedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(MainActivity.this,MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
